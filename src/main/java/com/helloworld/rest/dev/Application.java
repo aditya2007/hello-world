cdb3fb0a34d9ec8c77dabe24f2a32e67b5304420
212590467 <yogananda.govindagowda@ge.com>
2018-01-24 16:23:15 -0800

    Created a heelo-world project structure

---
+++ src/main/java/com/helloworld/rest/dev/Application.java
@@ -0,0 +1,12 @@
+package com.helloworld.rest.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
