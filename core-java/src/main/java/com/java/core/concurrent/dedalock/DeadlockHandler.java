fa01a1b3beb29e47b9783fea1aff8149462debbf
Yogananda <yoga@MacBook-Pro.local>
2018-03-20 20:03:15 -0700

    Removed un necessary files

--- core-java/src/main/java/com/java/core/concurrent/dedalock/DeadlockHandler.java
+++
@@ -1,7 +0,0 @@
-package com.java.core.concurrent.dedalock;
-
-import java.lang.management.ThreadInfo;
-
-public interface DeadlockHandler {
-	void handleDeadlock(final ThreadInfo[] deadlockedThreads);
-}
